6f1f836275921e663665a908800924d063a4e062
MJ <84123624+403rbidden@users.noreply.github.com>
2023-11-09 03:59:21 +0100

    Update Ejercicio1.js

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -7,9 +7,17 @@ var apellido = prompt("Ingresa tu apellido:");
 // Solicitar edad y convertir a número
 var edad = parseInt(prompt("Ingresa tu edad:"));
 
+/* 
 // Saludar y verificar si es mayor de edad
 if (edad >= 18) {
     alert("Hola, " + nombre + " " + apellido + ". Eres mayor de edad.");
 } else {
     alert("Hola, " + nombre + " " + apellido + ". Eres menor de edad.");
-}
+} 
+*/
+
+// Determinar si es mayor de edad utilizando un operador condicional ternario
+var mensaje = "Hola, " + nombre + " " + apellido + ". Eres " + (edad >= 18 ? "mayor" : "menor") + " de edad.";
+
+// Mostrar el mensaje
+alert(mensaje);
