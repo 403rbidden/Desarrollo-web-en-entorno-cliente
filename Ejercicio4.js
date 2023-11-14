f1eab5b236fce7e6cf96608e512a0155ff767975
MJ <84123624+403rbidden@users.noreply.github.com>
2023-11-14 01:20:18 +0100

    Update Ejercicio4.js

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,15 +1,15 @@
-//FECHA ACTUAL FORMATEADA
+//FECHA Y HORA ACTUAL FORMATEADA
 
 // Obtener la fecha actual
 const fechaActual = new Date();
 
-// Obtener día
+// Obtener dia
 const dia = fechaActual.getDate().toString().padStart(2, '0');
 // Obtener mes
-const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0'); 
-    // Se suma 1 porque los meses van de 0 a 11
-// Obtener año
-const anio = fechaActual.getFullYear();
+const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+// Se suma 1 porque los meses van de 0 a 11
+// Obtener ano
+const ano = fechaActual.getFullYear();
 
 // Obtener la hora
 const horas = fechaActual.getHours().toString().padStart(2, '0');
@@ -19,11 +19,15 @@ const minutos = fechaActual.getMinutes().toString().padStart(2, '0');
 const segundos = fechaActual.getSeconds().toString().padStart(2, '0');
 
 // Construir los mensajes con el formato requerido
-    // dd/MM/YYYY
-const mensajeFecha = `Fecha actual: ${dia}/${mes}/${anio}`;
-    // hh:mm:ss
+// dd/MM/YYYY
+const mensajeFecha = `Fecha actual: ${dia}/${mes}/${ano}`;
+// hh:mm:ss
 const mensajeHora = `Hora actual: ${horas}:${minutos}:${segundos}`;
 
-// Mostrar los mensajes
-console.log(mensajeFecha);
-console.log(mensajeHora);
+// Mostrar los mensajes por consola o F12
+/*console.log(mensajeFecha);
+ console.log(mensajeHora);*/
+
+// Mostrar los mensajes 
+document.getElementById('mensajeFecha').textContent = mensajeFecha;
+document.getElementById('mensajeHora').textContent = mensajeHora;
